cbda0cc4888abb5139a9a01d813df19e25d7d7db
MagicConch <350043743@qq.com>
2017-05-03 22:39:09 +0900

    ..

--- flashsubBack/models/db.js
+++ flashsubBack/models/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect("mongodb://localhost:27017/flashsub");
+const db = mongoose.connect("mongodb://127.0.0.1:27017/flashsub");
 
 
 module.exports = db;
